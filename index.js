a80b172cc2c246dd559aaf88213b516d4c61b2da
FCarter09 <fc_vstate@yahoo.com>
2023-11-09 17:06:14 -0500

    generate html template feature

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // receives inquire file
 const inquirer = require('inquirer');
 
-// file that uses fs to write output to an html file
-const {writeFile, copyFile} = require('./utils/generate-page');
 
 //receives page-template
 const generatePage = require('./src/page-template');
+
+// import fs to write file
+const fs  = require('fs')
 
 
 // import Classes of Employees
@@ -13,13 +14,16 @@ const Manager = require('./lib/Manager')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
-
+//finalHTML will store all HTML of the employees in the company
+// this is an accumulator variable. Instead of accumulating numbers it accumulates text (strings)
+let finalHTML = []
+let managerHTML = ''
+let engineerHTML = ''
+let internHTML = ''
 
 const askQuestions = () => {
 
-    //finalHTML will store all HTML of the employees in the company
-    // this is an accumulator variable. Instead of accumulating numbers it accumulates text (strings)
-    let finalHTML = ''
+  
 
     // ask what type of employee to create
     inquirer.prompt({
@@ -67,10 +71,18 @@ const askQuestions = () => {
                 const managerData = generatePage.createManagerHTMLCard(newManager)
 
                 // add managerData to the finalHTML
-                finalHTML += managerData
+                managerHTML += managerData
 
-                // callback askQuestions() to return user back to questions
-                askQuestions();
+                // console.log('Manager HTML:', managerHTML);
+
+                finalHTML.push(managerHTML)
+
+                // console.log('Final HTML:', finalHTML);
+
+
+                askQuestions()
+
+               
             })
         } else if (userChoice.employeeType === 'Engineer') {
             inquirer.prompt([
@@ -105,8 +117,14 @@ const askQuestions = () => {
                 const engineerData = generatePage.createEngineerHTMLCard(newEngineer)
 
                 // add engineerData to the finalHTML
-                finalHTML += engineerData
-                
+                engineerHTML += engineerData
+
+                // console.log('Engineer HTML:', engineerHTML)
+
+                finalHTML.push(engineerHTML)
+
+                // console.log('Final HTML:', finalHTML);
+
                 // callback askQuestions() to return user back to questions
                 askQuestions();
     
@@ -144,24 +162,50 @@ const askQuestions = () => {
                 const internData = generatePage.createInternHTMLCard(newIntern)
 
                 // add internData to the finalHTML
-                finalHTML += internData    
+                internHTML += internData  
+                
+                // console.log('Intern HTML:', internHTML)
+                
+                finalHTML.push(internHTML)
+
+                // console.log('Final HTML:', finalHTML);
+
+                askQuestions();
 
             })
+            }else if(userChoice.employeeType === 'Exit') {
+              // htmlTemplate is an array
+              // add finalHTML into htmlTemplate in correct place
+              generatePage.HTMLtemplate.splice(1, 0, finalHTML)
+              console.log(generatePage.HTMLtemplate);
 
-        }
-    })
+              // join all the items into the html template array
+              let generatedHTML = generatePage.HTMLtemplate.join()
 
-        // after you ask all the questions you have final html
-        // create final HTML files using finalHTML variable
-        // use html template from page-template.js
+
+              // deletes left over commas
+              generatedHTML = generatedHTML.replace(/,/g,'');
+              console.log('Generates HTML', generatedHTML);
+
+
+              //after asking all questions, use fs to write output to an html file
+              // fs.writeFile creates finalHTML after 'exit' is selected
+              fs.writeFile('./dist/TeamProfile.html', generatedHTML, (error) =>
+                error ? console.log('Error!') : console.log(`Output written to file: TeamProfile.html`));  //error handling is a required argument
+
+              // fs copyFile copies styles.css from 'src' folder to styles.css in 'dist' folder 
+              fs.copyFile('./src/style.css', './dist/style.css', (error) => 
+                error ? console.log('Error!') : console.log(" './src/style.css' copied to './dist/style.css' "));
+               
+            }
         
-
+    })
+      
     
 }
 
 
-//after asking all questions, use fs to write output to an html file
-// you will write finalHTML to an output file called 'generate-page' in utils folder
+// call askQuestions function
  askQuestions()
 
 
@@ -169,23 +213,10 @@ const askQuestions = () => {
 
 
 
-    // NOTE: Commented code below could possibly be used to execute app in asynchronous order using '.then' promises
 
-    // .then(pageHTML => {
-    //     return writeFile(pageHTML);
-    // })
 
-    // .then(writeFileResponse => {
-    //     console.log(writeFileResponse);
-    //     return copyFile();
-    // })
 
-    // .then(copyFileResponse => {
-    //     console.log(copyFileResponse);
-    // })
 
-    // .catch(err => {
-    //     console.log(err);
-    // });
+
 
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,1 +1,78 @@
 
+const createManagerHTMLCard = (manager) => {
+    const managerHTML = `
+    <div class="managers-card">
+        <h3 class='employee-name'>${manager.name}</h3>
+        <h4>${manager.role}</h4>
+        <p><span>ID:</span>${manager.id}</p>
+        <p><span>Email:</span><a href="mailto:${manager.email}">${manager.email}</a></p>
+        <p><span>Office Number:</span>${manager.officeNumber}</p>
+    </div>
+    `
+    return managerHTML
+}
+
+//Create engineer html card
+const createEngineerHTMLCard = (engineer) => {
+    const engineerHTML = `
+    <div class="engineers-card">
+        <h3 class='employee-name'>${engineer.name}</h3>
+        <h4>${engineer.role}</h4>
+        <p><span>ID:</span>${engineer.id}</p>
+        <p><span>Email:</span><a href="mailto:${engineer.email}">${engineer.email}</a></p>
+        <p><span>Github:</span><a href="https://github.com/${engineer.github}">Github Profile</a></p>
+    </div>
+    `
+    return engineerHTML
+}
+
+// Create intern html card
+const createInternHTMLCard = (intern) => {
+    const internHTML = `
+    <div class="interns-card">
+        <h3 class='employee-name'>${intern.name}</h3>
+        <h4>${intern.role}</h4>
+        <p><span>ID:</span>${intern.id}</p>
+        <p><span>Email:</span><a href="mailto:${intern.email}">${intern.email}</a></p>
+        <p><span>School:</span>${intern.school}</p>
+    </div>
+    `
+    return internHTML
+}
+
+const HTMLtemplate = [`
+     <!DOCTYPE html> 
+     <html lang="en"> 
+     <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <title>Team Portfolio</title>
+       <link rel="stylesheet" href="style.css">
+     </head>
+   
+     <body>
+        <header>
+            Team Portfolio
+        </header>
+        <main class="employee-section">`,
+
+        //PUT EMPLOYEE HTML HERE (manager, engineer, intern)
+        //EMPLOYEE DATA FROM INDEX.js FILE IS CALLED 'finalHTML'
+        
+
+        `</main>
+     </body>
+     </html>
+`];
+
+
+
+
+// export function to generate entire page
+module.exports = {
+    createManagerHTMLCard,
+    createEngineerHTMLCard,
+    createInternHTMLCard,
+    HTMLtemplate,
+}
